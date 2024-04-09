3bdd8acc878c3291efc825742779f74e2468a169
Prajwal B S <prajwaljhon@gmail.com>
2024-04-09 22:39:27 +0530

    JOINS AND TYPE CONVERSION

---
+++ python/Typecasting.py
@@ -0,0 +1,506 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Converting to int")
+
+
+# In[4]:
+
+
+int(9.999)
+
+
+# In[5]:
+
+
+int(True)
+
+
+# In[6]:
+
+
+int(7+9j)
+
+
+# In[7]:
+
+
+int('iuygiy')
+
+
+# In[8]:
+
+
+int('1234')
+
+
+# In[9]:
+
+
+int("1234")
+
+
+# In[10]:
+
+
+int('12.36')
+
+
+# In[11]:
+
+
+int([1,6,7])
+
+
+# In[12]:
+
+
+int((1,2,5))
+
+
+# In[13]:
+
+
+int({1,2,3,4})
+
+
+# In[14]:
+
+
+int({'p':1,'gh':9})
+
+
+# In[15]:
+
+
+print("concering to float")
+
+
+# In[16]:
+
+
+float(1)
+
+
+# In[17]:
+
+
+float(1/2)
+
+
+# In[18]:
+
+
+float(1.2)
+
+
+# In[19]:
+
+
+float(True)
+
+
+# In[20]:
+
+
+float(1+1j)
+
+
+# In[21]:
+
+
+float("123")
+
+
+# In[22]:
+
+
+float("123.45")
+
+
+# In[23]:
+
+
+float("dlknj2ss\")
+
+
+# In[24]:
+
+
+float([1,2,3])
+
+
+# In[25]:
+
+
+float({1,4,5})
+
+
+# In[26]:
+
+
+flot({'1':2,'3':4})
+
+
+# In[27]:
+
+
+print("Converting to boolean")
+
+
+# In[28]:
+
+
+bool(1)
+
+
+# In[29]:
+
+
+bool(12)
+
+
+# In[30]:
+
+
+bool(-22)
+
+
+# In[31]:
+
+
+bool()
+
+
+# In[32]:
+
+
+bool("")
+
+
+# In[33]:
+
+
+bool({})
+
+
+# In[34]:
+
+
+bool(set())
+
+
+# In[37]:
+
+
+bool(1+2j)
+
+
+# In[38]:
+
+
+print("Converting to complex")
+
+
+# In[39]:
+
+
+complex(1)
+
+
+# In[40]:
+
+
+complex(1.0)
+
+
+# In[41]:
+
+
+complex(1.2)
+
+
+# In[43]:
+
+
+complex(1,2)
+
+
+# In[44]:
+
+
+complex(2.2,2.2)
+
+
+# In[45]:
+
+
+complex(1+2j)
+
+
+# In[46]:
+
+
+complex(1+2j,1+2j)
+
+
+# In[47]:
+
+
+complex("2")
+
+
+# In[48]:
+
+
+complex("2","3")
+
+
+# In[49]:
+
+
+complex()4,8j
+
+
+# In[50]:
+
+
+complex(4,8j)
+
+
+# In[52]:
+
+
+complex("3",9)
+
+
+# In[53]:
+
+
+bool()
+
+
+# In[54]:
+
+
+thyh
+
+
+# In[55]:
+
+
+print("Complex won't suppoprt Collection dtattype conversion")
+
+
+# In[56]:
+
+
+print("Converting to string")
+
+
+# In[57]:
+
+
+str(1)
+
+
+# In[58]:
+
+
+str(2.0)
+
+
+# In[59]:
+
+
+str(True)
+
+
+# In[60]:
+
+
+str(1+7j)
+
+
+# In[61]:
+
+
+str([1,2])
+
+
+# In[62]:
+
+
+str({1,2,3})
+
+
+# In[63]:
+
+
+str((2,3))
+
+
+# In[64]:
+
+
+str({'1':2,'2':3})
+
+
+# In[65]:
+
+
+print("String supports all kinds of conversion")
+
+
+# In[1]:
+
+
+print("converting to list")
+
+
+# In[2]:
+
+
+list(1)
+
+
+# In[3]:
+
+
+list(1,2,3)
+
+
+# In[4]:
+
+
+list((1,2,3))
+
+
+# In[5]:
+
+
+list({1,2,3})
+
+
+# In[8]:
+
+
+list({'1':2,'3':4})
+
+
+# In[9]:
+
+
+print('list wont support single value data type')
+
+
+# In[10]:
+
+
+print('converting to set')
+
+
+# In[11]:
+
+
+set([1,2,3])
+
+
+# In[13]:
+
+
+set('sett')#unorder data type
+
+
+# In[14]:
+
+
+set((1,2,3))
+
+
+# In[15]:
+
+
+set({'1':2,'3':4})
+
+
+# In[16]:
+
+
+set(1,2,4)
+
+
+# In[17]:
+
+
+tuple('str')
+
+
+# In[18]:
+
+
+tuple([1,2,3])
+
+
+# In[19]:
+
+
+tuple({1,2,3})
+
+
+# In[20]:
+
+
+tuple({'1':2,'3':4})
+
+
+# In[21]:
+
+
+tuple(True)
+
+
+# In[22]:
+
+
+dict('wert')
+
+
+# In[23]:
+
+
+dict('1234')
+
+
+# In[28]:
+
+
+dict((11,))
+
+
+# In[35]:
+
+
+dict(['11',[22,33]])
+
+
+# In[31]:
+
+
+dict(['12',[3,4]])
+
+
+# In[36]:
+
+
+dict(('11',[1,2]))
+
+
+# In[ ]:
+
+
+
+
