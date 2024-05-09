def833cc40c5a825e960e98ed07195ce3bb21df2
Prajwal B S <prajwaljhon@gmail.com>
2024-05-09 21:10:57 +0530

    USERS FORM and pattern

--- python/PAttern/kind of star.py
+++ python/PAttern/kind of star.py
@@ -1,6 +1,6 @@
-var=7
+var=8
 for l in range(1,var):
-    print(" "*(var-l)+"*" * ((2*l)-1))
+    print(" "*(var-l)+" * " * ((2*l)-1))
 
 for l in range(var,0,-1):
-    print(" "*(var-l)+"*" * ((2*l)-1))
+    print(" "*(var-l)+" * " * ((2*l)-1))
