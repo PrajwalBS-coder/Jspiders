55662b6f9c5a9444988d0fecda2187a239b237e0
Prajwal B S <prajwaljhon@gmail.com>
2024-06-26 21:34:34 +0530

    leert

--- python/Leet Code/1550. Three Consecutive Odds.py
+++ python/Leet Code/1550. Three Consecutive Odds.py
@@ -1,4 +1,4 @@
-def con(arr):
+"""def con(arr):
     for i in range(len(arr)-2):
         #print(arr[i],arr[i]+1,arr[i]+2)
         #print(i%2==1),((i+1)%2==1),((i+2)%2==1)
@@ -8,3 +8,16 @@ def con(arr):
 arr = [1,2,34,3,4,5,7,23,12]
 print(con(arr))
 
+"""
+arr = [1,2,34,3,4,5,7,23,12]
+c=0
+for i in range(len(arr)):
+    if(arr[i]%2==1):
+        c+=1
+        if(c==3):
+            print(True)
+            break
+    else:
+        c=0
+else:
+    print(False)
