9aebae90721d306a0c788d65f113f3681eab070a
Prajwal B S <prajwaljhon@gmail.com>
2024-07-18 14:38:56 +0530

    268

--- python/Leet Code/268. Missing Number.py
+++ python/Leet Code/268. Missing Number.py
@@ -1,5 +1,17 @@
-nums = [9,6,4,2,3,5,7,0,1]
+# nums = [9,6,4,2,3,5,7,0,1]
+# nums.sort()
+# l=[i for i in range(nums[0],nums[-1]+1)]
+# op=(str(set(l)-set(nums)))
+# print(op.strip("{}"))
+
+
+nums = [0,1]
 nums.sort()
 l=[i for i in range(nums[0],nums[-1]+1)]
-op=(str(set(l)-set(nums)))
-print(op.strip("{}"))
+if(len(l)==2):
+    print(l[-1]+1)
+for i in l:
+    if i not in nums:
+        print(i,end=" ")
+        break
+# print(l)
