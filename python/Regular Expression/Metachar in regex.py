03964fb05697b0eb72c65868edf0353ac0d93c31
Prajwal B S <prajwaljhon@gmail.com>
2024-07-26 22:05:32 +0530

    meta character

---
+++ python/Regular Expression/Metachar in regex.py
@@ -0,0 +1,4 @@
+import re as r
+s="Hello Iam pr@jwal B.$ \nAMin"
+print(r.findall('.',s))#lists every character in list excluding "\n"
+print(r.findall('^He',s))#it works like starts with o/p will be in list if given pattern exist else [] empty list
