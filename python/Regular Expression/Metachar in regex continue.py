2334b3d7bdd5053b6d14606b0972eadbecedf750
Prajwal B S <prajwaljhon@gmail.com>
2024-07-26 22:35:29 +0530

    metca char in reg ex continue...

---
+++ python/Regular Expression/Metachar in regex continue.py
@@ -0,0 +1,3 @@
+import re as r
+s="godd good gooooood goooof g"
+print(r.findall('g.*d',s))#litte confusing
--- python/Regular Expression/Metachar in regex.py
+++ python/Regular Expression/Metachar in regex.py
@@ -2,3 +2,5 @@ import re as r
 s="Hello Iam pr@jwal B.$ \nAMin"
 print(r.findall('.',s))#lists every character in list excluding "\n"
 print(r.findall('^He',s))#it works like starts with o/p will be in list if given pattern exist else [] empty list
+print(r.findall('n$',s))#it works like ends with o/p will be in list if given pattern exist else [] empty list
+print(r.findall('H*o',s))
