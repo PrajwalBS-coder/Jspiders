a63adad249e57d44f848991b71786da905b5c6af
Prajwal B S <prajwaljhon@gmail.com>
2024-07-26 21:05:45 +0530

    1523

--- python/Leet Code/1523. Count Odd Numbers in an Interval Range.py
+++ python/Leet Code/1523. Count Odd Numbers in an Interval Range.py
@@ -1,7 +1,14 @@
+# low = 3
+# high = 7
+# c=0
+# for i in range(low,high+1):
+#     if(i%2==1):
+#         c+=1
+# print(c)
+
 low = 3
 high = 7
-c=0
-for i in range(low,high+1):
-    if(i%2==1):
-        c+=1
-print(c)
+odd = (high - low) // 2
+if low % 2 == 1 or high % 2 == 1:
+    odd += 1
+print(odd)
