c20d061019af0e7a4d59ed8c86fc8ede406ae189
Prajwal B S <prajwaljhon@gmail.com>
2024-08-02 14:06:23 +0530

    476 updated

--- python/Leet Code/476. Number Complement.py
+++ python/Leet Code/476. Number Complement.py
@@ -2,9 +2,10 @@ num = 5
 n1=(str(bin(num)).strip("0b"))
 print(n1)
 n2=''
-for i in range(len(n1)-1):
+for i in range(len(n1)):
     if n1[i]=='1':
         n2+='0'
     else:
         n2+='1'
-print(int(n2))
+n2+=' '
+print(int(n2,2))
