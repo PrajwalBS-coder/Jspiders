8ab02016fc762fc1fa86c59f0f3ea608fd925542
Prajwal B S <prajwaljhon@gmail.com>
2024-08-03 12:29:02 +0530

    167

---
+++ python/Leet Code/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,10 @@
+numbers = [2,3,4]
+target = 6
+l=[]
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if(numbers[i]+numbers[j]==target):
+            l.append(i+1)
+            l.append(j+1)
+            break
+print(l)
