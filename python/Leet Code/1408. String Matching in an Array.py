71bd6dbddf7c8d391e65bf4e7c92d3a9c8784435
Prajwal B S <prajwaljhon@gmail.com>
2024-08-06 23:06:23 +0530

    1408 new code

--- python/Leet Code/1408. String Matching in an Array.py
+++ python/Leet Code/1408. String Matching in an Array.py
@@ -1,8 +1,10 @@
 words =["mass","as","hero","superhero"]
-a=[]
-for word in words:
-    for i in words:
-        if word in i and word!=i:
-            a.append(word)
-            break
-print(a)
+# a=[]
+# for word in words:
+#     for i in words:
+#         if word in i and word!=i:
+#             a.append(word)
+#             break
+# print(a)
+c=" ".join(words)
+print([i for i in words if c.count(i)>1])
