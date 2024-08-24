9152c686edb42113a0af291399c323fbd9b0c0b1
Prajwal B S <prajwaljhon@gmail.com>
2024-08-24 10:17:37 +0530

    564

---
+++ python/Leet Code/564. Find the Closest Palindrome.py
@@ -0,0 +1,31 @@
+n = "1"
+c1,c2=0,0
+l=[]
+l2=[]
+for i in range(int(n)-1,-1,-1):
+    c1+=1
+    if(str(i)==str(i)[::-1]):
+        # print(i)
+        l.append(str(i))
+        # c1+=1
+        break
+# k=int(n)
+# while(len(l2)!=1):
+#     if(str(k)==str(k)[::-1]):
+#         # print(k)
+#         l2.append(str(k))
+#         c2+=1
+#         k+=1
+for i in range(int(n)+1,int(n)*2):
+    c2+=1
+    if(str(i)==str(i)[::-1]):
+        # print(i)
+        l2.append(str(i))
+        break
+print(c1,c2)
+if(c1==c2):
+    print("".join(l))
+elif(c1>c2):
+    print("".join(l2))
+else:
+    print("".join(l))
