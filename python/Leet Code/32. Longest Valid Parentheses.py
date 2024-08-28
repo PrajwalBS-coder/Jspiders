8ef9ba206225a2907fd6b2696055f520388b1c08
Prajwal B S <prajwaljhon@gmail.com>
2024-08-28 19:23:25 +0530

    32 dited

--- python/Leet Code/32. Longest Valid Parentheses.py
+++ python/Leet Code/32. Longest Valid Parentheses.py
@@ -1,10 +1,13 @@
 s =  ""
 
-ob=0
-cb=0
+# ob=0
+# cb=0
+# for i in s:
+#     if i=='(':
+#         ob+=1
+#     if i==')':
+#         cb+=1
+# print(abs(ob-cb)*2)
 for i in s:
     if i=='(':
-        ob+=1
-    if i==')':
-        cb+=1
-print(abs(ob-cb)*2)
+        
