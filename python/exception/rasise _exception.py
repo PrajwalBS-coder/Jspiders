daafc30ae11741c7909b1eb9b253bda40c1465c4
Prajwal B S <prajwaljhon@gmail.com>
2024-09-25 22:20:32 +0530

    Raise Exception

---
+++ python/exception/rasise _exception.py
@@ -0,0 +1,6 @@
+x=0
+if not x:
+    raise Exception('Erro')
+# As a Python developer you can choose to throw an exception if a condition occurs.
+
+# To throw (or raise) an exception, use the raise keyword.
