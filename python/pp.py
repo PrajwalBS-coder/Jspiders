761b5b82afad754ecd4048f60a541e389e8297a3
Prajwal B S <prajwaljhon@gmail.com>
2024-12-13 22:56:38 +0530

    Inspiron Interview

---
+++ python/pp.py
@@ -0,0 +1,8 @@
+import request
+try:
+    r=request.get("https://chatgpt.com/c/67595793-03cc-8011-8dc0-11ed4ac6be39/key='khadhs'/usn='legendamin008'&passwd='123456'")
+    if r.status_code!='200':
+        raise Exception
+
+except Exception as e :
+    print("An error occurred: ",e)
---
+++ python/ss.py
@@ -0,0 +1,9 @@
+a=[2,4,5,8,1,55,7,8]
+a.sort()
+a.reverse()
+op=[]
+us=int(input("Enter The Size"))
+for i in range(us):
+    op.append(a[i])
+print(op)
+
