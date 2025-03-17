554ac56f30537c37fdc9c56f6454ceb27c7bb9dd
Prajwal B S <prajwaljhon@gmail.com>
2025-03-17 19:24:55 +0530

    134 / 134 testcases passed differnt solution

--- python/Leet Code/2206. Divide Array Into Equal Pairs.py
+++ python/Leet Code/2206. Divide Array Into Equal Pairs.py
@@ -1,10 +1,16 @@
 nums = [3,2,3,2,2,2]
-if(len(nums)%2!=0):
-    print("False")
+# if(len(nums)%2!=0):
+#     print("False")
+# else:
+#     for i in nums:
+#         if(nums.count(i)%2!=0):
+#             print(False)
+#             break
+#     else:
+#         print(True)
+for i in nums:
+    if nums.count(i)%2!=0:
+        print(nums.count(i))
+        print( False)
 else:
-    for i in nums:
-        if(nums.count(i)%2!=0):
-            print(False)
-            break
-    else:
-        print(True)
+    print(True)
